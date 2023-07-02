567e00075f7bd0042288b2551b20b30376f0931e
Mohamed Sarhan <mohsarhan88@gmail.com>
2023-07-02 13:44:20 +0300

    change fields in signup

--- src/app/modules/auth-module/signup/signup.component.ts
+++ src/app/modules/auth-module/signup/signup.component.ts
@@ -20,12 +20,12 @@ export class SignupComponent {
    */
     errors: any;
     myfile:any
+    userRole:any="MENTEE"
     data: FormData = new FormData();
     form: FormGroup = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      username: new FormControl('', [Validators.required]),
-      firstName: new FormControl('', [Validators.required]),
-      lastName: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+   
       // mobile: new FormControl('', [
       //   Validators.required,
       //   Validators.pattern('^(011|012|010)([0-9]+){8}$'),
@@ -56,23 +56,11 @@ export class SignupComponent {
     get isLoggedIn() {
       return this.auth;
     }
-    get getUserName() {
-      return this.form.controls['username'];
+    get getName() {
+      return this.form.controls['name'];
     }
 
-    get getAddress() {
-      return this.form.controls['address'];
-    }
 
-    get getMobile() {
-      return this.form.controls['mobile'];
-    }
-    get getFirstName() {
-      return this.form.controls['firstName'];
-    }
-    get getLastName() {
-      return this.form.controls['lastName'];
-    }
     get getImage() {
       return this.form.controls['image'];
     }
@@ -97,33 +85,35 @@ export class SignupComponent {
     }
 
     submit(e: Event) {
-      let code: number = 0;
-      let email: string = this.getEmail.value;
+
       e.preventDefault();
       if (this.form.status == 'VALID') {
-        console.log(this.getImage.value)
-        console.log(this.getUserName.value)
-        this.data.append('username', this.getUserName.value);
-        this.data.append('first_name', this.getFirstName.value);
-        this.data.append('last_name', this.getLastName.value);
-        this.data.append('email', this.getEmail.value);
+        
+        this.data.append('name', this.form.controls['name'].value);
+        this.data.append('email', this.form.controls['email'].value);
+        this.data.append('role', this.userRole);
         this.data.append('password', this.getPassword.value);
         this.data.append('password2', this.getPassword2.value);
-        this.data.append('phone', this.getMobile.value);
-        this.data.append('picture', this.myfile);
-        this.auth.register(this.data).subscribe({
-          next: (res: any) => {
-            console.log(res);
-            this.router.navigate(['/payment']);
-          },
-          error: (err) => {
-            console.log(err);
-            this.errors = err.error;
-          },
-        });
+
+        this.data.append('img_url', this.myfile);
+        console.log(this.data)
+        // this.auth.register(this.data).subscribe({
+        //   next: (res: any) => {
+        //     console.log(res);
+        //     this.router.navigate(['/payment']);
+        //   },
+        //   error: (err) => {
+        //     console.log(err);
+        //     this.errors = err.error;
+        //   },
+        // });
       } else {
         this.form.markAllAsTouched();
       }
-    }
 
+      console.log(this.data)
+    }
+setRole(role:any){
+this.userRole=role;
+}
 }
